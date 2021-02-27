5de4f7a52f5ec0c4adc49fa42917d536bc99661e
sorin <predaandrei18@stud.ase.ro>
2021-02-27 14:59:02 +0200

    Seminar 1 final

---
+++ Seminar1/src/ro/ase/cts/clase/Girafa.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.clase;
+
+public class Girafa  extends Animal{
+private int inaltime;
+	public Girafa(String nume,int intaltime) {
+		super(nume);
+		this.inaltime=intaltime;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getInaltime() {
+		return inaltime;
+	}
+	public void setInaltime(int inaltime) {
+		this.inaltime = inaltime;
+	}
+	
+
+}
